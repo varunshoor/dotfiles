d4bc86ce309c3932b481f2b1ba6beb5a61cd5799
Varun Shoor <github@varunshoor.com>
2024-09-16 16:45:24 +0530

    Improved config of Substitute plugin in Neovim

--- nvim/lua/plugins/editor/substitute.lua
+++ nvim/lua/plugins/editor/substitute.lua
@@ -7,6 +7,9 @@ return {
 
     vim.keymap.set("n", "rl", require("substitute").line, { noremap = true })
     vim.keymap.set("n", "R", require("substitute").eol, { noremap = true })
+
+    -- ri", rib etc.
+    vim.keymap.set("n", "r", require("substitute").operator, { noremap = true })
     vim.keymap.set("x", "r", require("substitute").visual, { noremap = true })
   end,
 }

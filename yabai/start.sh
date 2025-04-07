2b5ee82976e70742ab0ddc1d0e89049b394366f5
Varun Shoor <github@varunshoor.com>
2025-04-07 18:39:05 +0530

    Cleaned up yabai init and moved it to plist from zsh

---
+++ yabai/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo /opt/homebrew/bin/yabai --load-sa
+/opt/homebrew/bin/yabai --start-service
+/opt/homebrew/bin/skhd --start-service
--- zsh/end.zsh
+++ zsh/end.zsh
@@ -3,8 +3,3 @@ then
     # tmux attach -t workspace || tmux new -s workspace
     tmuxinator start workspace
 fi
-
-# Load yabai
-sudo yabai --load-sa
-yabai --start-service
-skhd --start-service

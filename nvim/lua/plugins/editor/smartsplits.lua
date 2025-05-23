50b400e408b309a5575067ba3f8fcf28f71f0259
Varun Shoor <github@varunshoor.com>
2025-05-23 11:18:26 +0530

    Improvements to TMux and Nvim re. navigation. Updates.

--- nvim/lua/plugins/editor/smartsplits.lua
+++ nvim/lua/plugins/editor/smartsplits.lua
@@ -15,6 +15,15 @@ return {
     { "<C-S-Down>", "<Plug>SmartSplitResizeDown", mode = { "n" }, desc = "Resize down" },
     { "<C-S-Up>", "<Plug>SmartSplitResizeUp", mode = { "n" }, desc = "Resize up" },
     { "<C-S-Right>", "<Plug>SmartSplitResizeRight", mode = { "n" }, desc = "Resize right" },
+
+    { "<C-h>", "<Plug>SmartSplitLeft", mode = { "n" }, desc = "Move cursor left" },
+    { "<C-j>", "<Plug>SmartSplitDown", mode = { "n" }, desc = "Move cursor down" },
+    { "<C-k>", "<Plug>SmartSplitUp", mode = { "n" }, desc = "Move cursor up" },
+    { "<C-l>", "<Plug>SmartSplitRight", mode = { "n" }, desc = "Move cursor right" },
+    { "<C-S-h>", "<Plug>SmartSplitResizeLeft", mode = { "n" }, desc = "Resize left" },
+    { "<C-S-j>", "<Plug>SmartSplitResizeDown", mode = { "n" }, desc = "Resize down" },
+    { "<C-S-k>", "<Plug>SmartSplitResizeUp", mode = { "n" }, desc = "Resize up" },
+    { "<C-S-l>", "<Plug>SmartSplitResizeRight", mode = { "n" }, desc = "Resize right" },
   },
   config = function()
     local smart_splits = require("smart-splits")
@@ -31,5 +40,17 @@ return {
     vim.keymap.set("n", "<C-S-Down>", smart_splits.resize_down, { silent = true })
     vim.keymap.set("n", "<C-S-Up>", smart_splits.resize_up, { silent = true })
     vim.keymap.set("n", "<C-S-Right>", smart_splits.resize_right, { silent = true })
+
+    -- moving between splits
+    vim.keymap.set("n", "<C-h>", smart_splits.move_cursor_left, { silent = true })
+    vim.keymap.set("n", "<C-j>", smart_splits.move_cursor_down, { silent = true })
+    vim.keymap.set("n", "<C-k>", smart_splits.move_cursor_up, { silent = true })
+    vim.keymap.set("n", "<C-l>", smart_splits.move_cursor_right, { silent = true })
+
+    -- smart resize
+    vim.keymap.set("n", "<C-S-h>", smart_splits.resize_left, { silent = true })
+    vim.keymap.set("n", "<C-S-j>", smart_splits.resize_down, { silent = true })
+    vim.keymap.set("n", "<C-S-k>", smart_splits.resize_up, { silent = true })
+    vim.keymap.set("n", "<C-S-l>", smart_splits.resize_right, { silent = true })
   end,
 }

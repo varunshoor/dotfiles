f4f5cc7bd9018e6e5a057965b12ac8b193154bc9
Varun Shoor <github@varunshoor.com>
2025-06-02 15:27:18 +0530

    Updated Nvim, Introduced Yazi cfg

--- nvim/lua/plugins/editor/snacks.lua
+++ nvim/lua/plugins/editor/snacks.lua
@@ -28,6 +28,7 @@ return {
        ---@type snacks.dashboard.Item[]
        keys = {
          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+         { icon = "󰪶 ", key = "y", desc = "Explore Files", action = ":Yazi toggle" },
          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },

af312392ccab5a2948fc62b2d2c0d6f3091fe0c6
Varun Shoor <github@varunshoor.com>
2025-06-03 21:30:22 +0530

    Updates, tweaked yabai cfg to match whats in docs

--- yabai/start.sh
+++ yabai/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# See: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#updating-to-the-latest-release
+yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
 sudo /opt/homebrew/bin/yabai --load-sa
 /opt/homebrew/bin/yabai --start-service
 /opt/homebrew/bin/skhd --start-service
